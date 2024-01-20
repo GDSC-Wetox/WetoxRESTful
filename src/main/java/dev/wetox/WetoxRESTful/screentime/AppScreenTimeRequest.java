c5765ae725dffe5cec9912084a200b8e34c3d5d6
Jiseok Son <jiseokson@gmail.com>
2024-01-20 21:38:52 +0900

    fix: 앱 스크린 타임 요청, 응답에 사용되는 json 필드이름 변경
    기존:
     - appName
     - appCategory
    
    변경:
     - name
     - category

--- src/main/java/dev/wetox/WetoxRESTful/screentime/AppScreenTimeRequest.java
+++ src/main/java/dev/wetox/WetoxRESTful/screentime/AppScreenTimeRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppScreenTimeRequest {
-    private String appName;
-    private String appCategory;
+    private String name;
+    private String category;
     private Double duration;
 }
