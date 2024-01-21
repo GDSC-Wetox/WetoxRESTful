53eb3bf801f85b4df07327875c6a81df965ba73d
Jiseok Son <jiseokson@gmail.com>
2024-01-21 11:38:54 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/dev/wetox/WetoxRESTful/screentime/ScreenTimeRepository.java
+++ src/main/java/dev/wetox/WetoxRESTful/screentime/ScreenTimeRepository.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface ScreenTimeRepository extends JpaRepository<ScreenTime, Long> {
     @Query("SELECT s FROM ScreenTime s WHERE s.user.id = :userId ORDER BY s.updatedDate DESC")
--- src/main/java/dev/wetox/WetoxRESTful/screentime/ScreenTimeService.java
+++ src/main/java/dev/wetox/WetoxRESTful/screentime/ScreenTimeService.java
@@ -7,7 +7,6 @@ import dev.wetox.WetoxRESTful.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
