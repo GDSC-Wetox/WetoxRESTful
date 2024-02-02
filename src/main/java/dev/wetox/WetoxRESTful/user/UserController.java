89fc2a3d940d4335da18b83c700e19da35a7c5c7
Jiseok Son <jiseokson@gmail.com>
2024-02-02 23:35:39 +0900

    fix: 이용되지 않았던 서비스 코드 searchFriendsByNickname를 컨트롤러에 연결

--- src/main/java/dev/wetox/WetoxRESTful/user/UserController.java
+++ src/main/java/dev/wetox/WetoxRESTful/user/UserController.java
@@ -5,8 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -19,5 +22,12 @@ public class UserController {
             @AuthenticationPrincipal User user
     ) {
         return ResponseEntity.ok(userService.retrieveProfile(user.getId()));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<UserResponse>> ser(
+            @RequestBody UserNicknameRequest reqeust
+    ) {
+        return ResponseEntity.ok(userService.searchFriendsByNickname(request.getNickname()));
     }
 }
