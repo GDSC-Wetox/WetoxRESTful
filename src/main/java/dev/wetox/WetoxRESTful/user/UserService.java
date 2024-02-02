9773f621f0ef9d56e6afb1da4ab480f8a145056a
Jiseok Son <jiseokson@gmail.com>
2024-02-02 20:53:23 +0900

    add: 동일한 닉네임으로 등록된 사용자 존재 질의 API
    새로 등록하고자 하는 사용자 닉네임이 이미 등록되었는지 확인하고자 할 때 이용.
    
    request:
    GET /auth/valid/nickname
    {
      "nickname": String
    }
    
    response:
    {
      "existed": Boolean
    }

--- src/main/java/dev/wetox/WetoxRESTful/user/UserService.java
+++ src/main/java/dev/wetox/WetoxRESTful/user/UserService.java
@@ -2,17 +2,13 @@ package dev.wetox.WetoxRESTful.user;
 
 import dev.wetox.WetoxRESTful.exception.MemberNotFoundException;
 import dev.wetox.WetoxRESTful.image.ImageService;
-import dev.wetox.WetoxRESTful.screentime.ScreenTime;
-import dev.wetox.WetoxRESTful.screentime.ScreenTimeRepository;
 import dev.wetox.WetoxRESTful.screentime.ScreenTimeResponse;
 import dev.wetox.WetoxRESTful.screentime.ScreenTimeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -31,5 +27,10 @@ public class UserService {
                 .totalDuration(screenTimeResponse.getTotalDuration())
                 .profileImage(imageService.getImageUrl(user.getProfileImageUUID()))
                 .build();
+    }
+
+    public UserDuplicatedConfirmResponse checkNicknameDuplicated(String nickname) {
+        Optional<User> user = userRepository.findByNickname(nickname);
+        return UserDuplicatedConfirmResponse.build(user.isPresent());
     }
 }
